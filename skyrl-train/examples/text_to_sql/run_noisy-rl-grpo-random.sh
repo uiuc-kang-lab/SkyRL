ad7051379ecb72af072b7b89619d1c98e89d873b
Yuxuan Zhu <yxx404@illinois.edu>
2025-09-09 01:14:22 -0400

    tweak parameters for random reward

--- skyrl-train/examples/text_to_sql/run_noisy-rl-grpo-random.sh
+++ skyrl-train/examples/text_to_sql/run_noisy-rl-grpo-random.sh
@@ -10,7 +10,7 @@ set -x
 DATA_DIR="$HOME/noisy-rl/data"
 DB_PATH="$HOME/noisy-rl/data"
 CKPT_PATH="$HOME/ckpts/skyrl_noisy_random_sql_7B_ckpt"
-EXPORT_PATH="$HOME/exports/skyrl_noisy_sql_7B_export"
+EXPORT_PATH="$HOME/exports/skyrl_noisy_random_sql_7B_export"
 
 echo "Using data from $DATA_DIR"
 
@@ -46,8 +46,8 @@ uv run --isolated --extra vllm -m skyrl_train.entrypoints.main_base \
   trainer.policy.optimizer_config.lr=1.0e-6 \
   trainer.policy_mini_batch_size=64 \
   trainer.algorithm.use_kl_loss=false \
-  trainer.ckpt_interval=150 \
-  trainer.hf_save_interval=150 \
+  trainer.ckpt_interval=100 \
+  trainer.hf_save_interval=-1 \
   trainer.dump_data_batch=true \
   trainer.export_path=$EXPORT_PATH \
   generator.backend=vllm \
@@ -68,7 +68,7 @@ uv run --isolated --extra vllm -m skyrl_train.entrypoints.main_base \
   environment.skyrl_gym.text2sql.random_perturb=true \
   trainer.logger="wandb" \
   trainer.project_name="noisy-rl" \
-  trainer.run_name="noisy-longrun" \
+  trainer.run_name="noisy-random-reward" \
   trainer.resume_mode=latest \
   trainer.ckpt_path=$CKPT_PATH \
   trainer.eval_batch_size=1024 \
