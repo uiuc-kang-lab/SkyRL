06211d9f329655d318112445dbbf64c6f6ef1fed
Yuxuan Zhu <yxx404@illinois.edu>
2026-03-14 03:08:54 +0000

    add tinker backend script

---
+++ examples/train/eurus_rl_math/start_tinker_backend.sh
@@ -0,0 +1,4 @@
+uv run --extra gpu --extra tinker -m skyrl.tinker.api \
+ --base-model Qwen/Qwen3.5-4B \
+ --backend-config '{"max_lora_adapters": 30, "max_lora_rank": 1, "tensor
+_parallel_size": 2, "train_micro_batch_size": 1, "sample_max_num_sequences": 64, "shard_attention_heads": false}' > out.log
