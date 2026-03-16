8707b60d19f20bb814e06fe415c85af0d310a3fc
Yuxuan Zhu <yxx404@illinois.edu>
2026-03-16 15:00:39 +0000

    change parameters

--- examples/train/eurus_rl_math/start_tinker_backend.sh
+++ examples/train/eurus_rl_math/start_tinker_backend.sh
@@ -1,3 +1,3 @@
 uv run --extra gpu --extra tinker -m skyrl.tinker.api \
  --base-model Qwen/Qwen3.5-4B \
- --backend-config '{"max_lora_adapters": 1000, "max_lora_rank": 4, "tensor_parallel_size": 8, "train_micro_batch_size": 1, "sample_max_num_sequences": 64, "shard_attention_heads": false}' > out.log
+ --backend-config '{"max_lora_adapters": 2, "max_lora_rank": 4, "tensor_parallel_size": 8, "train_micro_batch_size": 1, "sample_max_num_sequences": 64, "shard_attention_heads": false}' > out.log
