3ea05e8c237e90f636721c61f4b6bf7a6b2b3892
Yuxuan Zhu <yxx404@illinois.edu>
2026-03-27 20:17:14 +0000

    add OPD script for Qwen3.5-2b model

--- examples/train/eurus_rl_math/run_eval_teacher.sh
+++ examples/train/eurus_rl_math/run_eval_teacher.sh
@@ -62,7 +62,7 @@ OPTIONAL_ARGS=()
 
 uv run --extra fsdp -m examples.train.eurus_rl_math.eval_teacher \
   --model_path "$MODEL_PATH" \
-  --data_path "$DATA_DIR/train_32k.parquet" \
+  --data_path "$DATA_DIR/train.parquet" \
   --num_gpus "$NUM_GPUS" \
   --max_prompt_length "$MAX_PROMPT_LENGTH" \
   --max_generate_length "$MAX_GENERATE_LENGTH" \
