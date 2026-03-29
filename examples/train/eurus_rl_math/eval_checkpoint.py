c9f97fd22ee37a4ec2b027933329d4208605ed62
Yuxuan Zhu <yxx404@illinois.edu>
2026-03-28 21:37:19 -0400

    update script for pre-fix caching

--- examples/train/eurus_rl_math/eval_checkpoint.py
+++ examples/train/eurus_rl_math/eval_checkpoint.py
@@ -198,6 +198,7 @@ async def run_eval(args: argparse.Namespace, model_path: str) -> dict:
         tokenizer=tokenizer,
         backend="vllm",
         inference_engine_enable_sleep=False,
+        engine_init_kwargs={"language_model_only": True},
     )
     client = InferenceEngineClient(engines, tokenizer, model_path, SkyRLLoraConfig(), ie_cfg)
 
